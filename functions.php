769f189725af1fdabd1a1e9893f318189db93d5b
imran <imran75hossainkhan@gmail.com>
2021-05-24 08:32:15 -0600

    word

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+require_once(get_template_directory().'/inc/enqueue.php');
+
+
+function theme_setpu()
+{
+	load_theme_textdomain('imran',get_template_directory_uri(),'/language');
+	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails' ,array('slider'));
+
+
+
+	register_nav_menus(array(
+		'primary_menu'				=> __('Primary Menu','imran'),
+		'footer_menu'				=> __('Footer Menu','imran')
+	));
+}
+add_action('after_setup_theme','theme_setpu');
+
+
+// custom post
+
+function imran_custom_posts()
+{
+	register_post_type('slider',array(
+		'labels'			=> array(
+			'name'				=> __('Slider','imran'),
+		   'singular_name'	    => __('Slider','imran')
+		),
+		'public'				=> true,
+		'supports'				=> array('title', 'editor', 'author', 'thumbnail')
+	));
+}
+add_action('init','imran_custom_posts');
+
+
+
